b45c29e047a08215b166a9965df3f180759bfdce
Facundo Cancela <facu.cancela.3@gmail.com>
2024-04-07 17:00:31 -0300

    MainMenu

---
+++ Assets/Scripts/Menu/Buttons/PlayButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public string nombreEscenaAJugar; // Nombre de la escena a la que quieres cambiar
+
+
+    public void CambiarEscena()
+    {
+        // Cambiar a la escena especificada
+        SceneManager.LoadScene(nombreEscenaAJugar);
+    }
+}
