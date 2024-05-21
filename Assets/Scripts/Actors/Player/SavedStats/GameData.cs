b2a5c3dada7a5b42a0d1575e83cebffbd4e40d3e
Facundo Cancela <facu.cancela.3@gmail.com>
2024-05-21 19:15:30 -0300

    Revert "Merge branch 'Arte' into Dev"
    This reverts commit bf74d946f26bf24e3872c1d4c052acf3c8852f3d, reversing
    changes made to d762e79566d8608a33438aeb643ebe10d1391ea7.

---
+++ Assets/Scripts/Actors/Player/SavedStats/GameData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    public int maxHealth;
+    public int damage;
+    public int money;
+}
