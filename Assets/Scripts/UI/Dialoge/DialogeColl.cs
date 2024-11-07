61c14428f5d0a6599efe985c1af6c600a139d128
Elian Embon <106391341+elianembon@users.noreply.github.com>
2024-11-07 17:39:27 -0300

    dis

--- Assets/Scripts/UI/Dialoge/DialogeColl.cs
+++ Assets/Scripts/UI/Dialoge/DialogeColl.cs
@@ -9,13 +9,16 @@ public class DialogeColl : MonoBehaviour
     public GameObject pressTObject; 
     private bool playerInRange = false;
     private DialogueManager dialogueManager;
+     
+    public GameObject Dialoge;
+
     private string dialogueText;
 
     public bool isMoloDialogue = false;
 
     private void Start()
     {
-        dialogueManager = FindObjectOfType<DialogueManager>(); 
+        dialogueManager = FindObjectOfType<DialogueManager>();
         if (dialogueManager == null)
         {
             Debug.LogError("DialogueManager no encontrado en la escena.");
@@ -24,11 +27,21 @@ public class DialogeColl : MonoBehaviour
         LoadDialogueFromFile();
         if (pressTObject != null)
         {
-            pressTObject.SetActive(false); 
+            pressTObject.SetActive(false);
         }
+
+        
+        Dialoge = GameObject.Find("Dialoge");
+
+        if (Dialoge != null)
+        {
+            
+            Dialoge.SetActive(false);
+        }
+
+
     }
 
-    
     private void LoadDialogueFromFile()
     {
         if (textFile != null)
@@ -75,19 +88,33 @@ public class DialogeColl : MonoBehaviour
     {
         if (playerInRange && Input.GetKeyDown(KeyCode.F))
         {
+
+
             if (pressTObject != null)
             {
-                pressTObject.SetActive(false); 
+                pressTObject.SetActive(false);
+                
             }
             if (dialogueManager != null)
             {
                 // Verificación si el diálogo está en curso
                 if (dialogueManager.IsDialogueActive)
                 {
+                    
+                    
+                        
+                    
+
                     dialogueManager.OnSpacePressed(); 
                 }
                 else
                 {
+                    
+                    if (Dialoge.activeSelf == true)
+                    {
+                        Dialoge.SetActive(false);
+                    }
+
                     dialogueManager.StartDialogue(dialogueText, isMoloDialogue); 
                 }
             }
